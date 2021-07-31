7020913c16ac4abbcb7601cfd89f39024735675c
yalcinPXL <yalcinPXL@gmail.com>
2021-07-31 17:38:30 +0200

    Add project files.

---
+++ BlazorEmailDemo/Startup.cs
@@ -0,0 +1,72 @@
+using BlazorEmailDemo.Data;
+using BlazorEmailDemo.EmailService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEmailDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<IEmailSender, EmailSender>(i =>
+                                                     new EmailSender(
+                                        //Parameters will come from appsettings
+                                        Configuration["EmailSender:Host"],
+                                        Configuration.GetValue<int>("EmailSender:Port"),
+                                        Configuration.GetValue<bool>("EmailSender:EnableSSL"),
+                                        Configuration["EmailSender:UserName"],
+                                        Configuration["EmailSender:Password"]
+                                        )
+    );
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
